e051f0c39d821e6e42262ec172417aab10a7ea4a
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-21 01:19:56 +0530

    Create Alter.sql

---
+++ Alter.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Grade CHAR(2),
+    Marks DECIMAL(5,2)
+);
+
+
+--viewing 
+SELECT * FROM Student;
+
+-- change table name 
+ALTER TABLE Student 
+RENAME TO BCA Students;
