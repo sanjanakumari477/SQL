3b6ea658edc206e9791cad76d44065665982c7a7
Sanjana Kumari <sksanjana1502@gmail.com>
2025-10-05 19:44:00 +0530

    Create Purchased_all.sql

---
+++ Purchased_all.sql
@@ -0,0 +1,10 @@
+--Find Customers Who Purchased All Products From a List
+Ans:-
+--Tables assumed: customers(customer_id), orders(order_id, customer_id, product_id)
+
+-- Suppose product_list = (1, 2, 3)
+SELECT customer_id
+FROM orders
+WHERE product_id IN (1, 2, 3)
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_id) = 3;
