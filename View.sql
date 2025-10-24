cb8c103dece289fd8192e7b535b4f38ab54791de
Sanjana Kumari <sksanjana1502@gmail.com>
2025-10-24 23:58:12 +0530

    Create View.sql

---
+++ View.sql
@@ -0,0 +1,10 @@
+What are views?
+
+Virtual tables created by a query. Do not store data themselves.
+
+Syntax:
+
+CREATE VIEW view_name AS
+SELECT column1, column2
+FROM table_name
+WHERE condition;
