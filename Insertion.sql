2165e63a837308e0d686d831adff4282ebf99c0a
Sanjana Kumari <sksanjana1502@gmail.com>
2025-10-25 23:59:16 +0530

    Update Insertion.sql

--- Insertion.sql
+++ Insertion.sql
@@ -1,2 +1,5 @@
 INSERT INTO Employees (ID, Name, Age, Department, Salary)
 VALUES (1, 'John Doe', 28, 'HR', 50000.00);
+(1, 'marine', 28, 's/w Engi.', 45000.00),
+(1, 'Janny', 28, 'manager', 60000.00),
+(1, 'ronyy', 28, 'HR', 550000.00);
