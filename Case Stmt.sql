0d09b121977f1fdfe6032205bd799a131e03ee58
Sanjana Kumari <sksanjana1502@gmail.com>
2025-10-26 09:36:06 +0530

    Add files via upload

---
+++ Case Stmt.sql
@@ -0,0 +1,12 @@
+Select * from students_db;
+
+--Case Stmt : It is used for conditinal logic , as like similar to to if-else in programing language. 
+
+SELECT 
+    first_name,
+    age,
+    CASE 
+        WHEN age >= 22 THEN 'Received Scholarship'
+        ELSE 'Not Received Scholarship'
+    END AS scholarship_status
+FROM students_db;
