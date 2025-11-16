a4d634d90e73cbc5e0cf5e2a0fe3bc4e4c4aea98
Sanjana Kumari <sksanjana1502@gmail.com>
2025-11-16 21:22:15 +0530

    Update InnerJoin1.sql

--- InnerJoin1.sql
+++ InnerJoin1.sql
@@ -1,4 +1,11 @@
-SELECT columns
+--SYNTAX
+ SELECT columns
 FROM table1
 INNER JOIN table2
 ON table1.common_column = table2.common_column;
+
+
+EXAMPLE- SELECT students.name, marks.marks
+FROM students
+INNER JOIN marks
+ON students.id = marks.student_id;
