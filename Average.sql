db29ec4bf5ba2d0f6d657bac2120543d1c4f5cab
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-21 21:35:17 +0530

    Update Average.sql

--- Average.sql
+++ Average.sql
@@ -1,4 +1,4 @@
---Q. Find employees who earn more than average salary
+-- Find employees who earn more than average salary
   SELECT * 
 FROM Employees
 WHERE Salary > (SELECT AVG(Salary) FROM Employees);
