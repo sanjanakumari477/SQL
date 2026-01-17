027a1c482e564a38055aeeaf4182346af2d85aca
Sanjana Kumari <sksanjana1502@gmail.com>
2026-01-17 21:14:47 +0530

    Update Purchased_All.sql

--- Purchased_All.sql
+++ Purchased_All.sql
@@ -1,7 +1,6 @@
 --Find Customers Who Purchased All Products From a List
 Ans:-
 --Tables assumed: customers(customer_id), orders(order_id, customer_id, product_id)
-
 -- Suppose product_list = (1, 2, 3)
 SELECT customer_id
 FROM orders
